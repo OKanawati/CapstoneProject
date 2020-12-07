3a2e24452d36bf9db3ead3cdc75667d08237ce7a
Thomas Forber <thomasforber@gmail.com>
2020-12-06 21:32:14 -0500

    Added updated Brands table initialization

--- CapstoneProject/src/main/resources/data.sql
+++ CapstoneProject/src/main/resources/data.sql
@@ -16,9 +16,40 @@ INSERT INTO user_roles (users_id, roles_id) VALUES (1, 2);
 INSERT INTO user_roles (users_id, roles_id) VALUES (2, 2);
 
 
-INSERT INTO Brand(brand_Name) VALUES ('IPHONE');
-INSERT INTO Brand(brand_Name) VALUES ('SAMSUNG');
-INSERT INTO Brand(brand_Name) VALUES ('LG');
+INSERT INTO Brand (id, brand_name) VALUES (1, 'ASUS');
+INSERT INTO Brand (id, brand_name) VALUES (2, 'BLACKBERRY');
+INSERT INTO Brand (id, brand_name) VALUES (3, 'ESSENTIAL PRODUCTS');
+INSERT INTO Brand (id, brand_name) VALUES (4, 'HTC');
+INSERT INTO Brand (id, brand_name) VALUES (5, 'HTC/GOOGLE');
+INSERT INTO Brand (id, brand_name) VALUES (6, 'HUAWEI');
+INSERT INTO Brand (id, brand_name) VALUES (7, 'HUAWEI/GOOGLE');
+INSERT INTO Brand (id, brand_name) VALUES (8, 'LENOVO');
+INSERT INTO Brand (id, brand_name) VALUES (9, 'LG');
+INSERT INTO Brand (id, brand_name) VALUES (10, 'LG/GOOGLE');
+INSERT INTO Brand (id, brand_name) VALUES (11, 'MEIZU');
+INSERT INTO Brand (id, brand_name) VALUES (12, 'MOTOROLA');
+INSERT INTO Brand (id, brand_name) VALUES (13, 'MOTOROLA/GOOGLE');
+INSERT INTO Brand (id, brand_name) VALUES (14, 'NEXTBIT');
+INSERT INTO Brand (id, brand_name) VALUES (15, 'MICROSOFT');
+INSERT INTO Brand (id, brand_name) VALUES (16, 'HMD GLOBAL');
+INSERT INTO Brand (id, brand_name) VALUES (17, 'NOKIA');
+INSERT INTO Brand (id, brand_name) VALUES (18, 'ONEPLUS');
+INSERT INTO Brand (id, brand_name) VALUES (19, 'OPPO');
+INSERT INTO Brand (id, brand_name) VALUES (20, 'PANASONIC');
+INSERT INTO Brand (id, brand_name) VALUES (21, 'GOOGLE');
+INSERT INTO Brand (id, brand_name) VALUES (22, 'SAMSUNG');
+INSERT INTO Brand (id, brand_name) VALUES (23, 'SAMSUNG/GOOGLE');
+INSERT INTO Brand (id, brand_name) VALUES (24, 'RAZER');
+INSERT INTO Brand (id, brand_name) VALUES (25, 'REALME');
+INSERT INTO Brand (id, brand_name) VALUES (26, 'XIAOMI');
+INSERT INTO Brand (id, brand_name) VALUES (27, 'SONY ERICSSON');
+INSERT INTO Brand (id, brand_name) VALUES (28, 'SONY MOBILE');
+INSERT INTO Brand (id, brand_name) VALUES (29, 'TCL');
+INSERT INTO Brand (id, brand_name) VALUES (30, 'TECNO MOBILE');
+INSERT INTO Brand (id, brand_name) VALUES (31, 'VIVO');
+INSERT INTO Brand (id, brand_name) VALUES (32, 'ZTE');
+INSERT INTO Brand (id, brand_name) VALUES (33, 'IPHONE');
+
 
 
 INSERT INTO Shop (name, phone_number, street, city, province, postal, owner_id)
@@ -33,12 +64,12 @@ INSERT INTO Shop (name, phone_number, street, city, province, postal, owner_id)
 	VALUES ('PcNcell', '(905) 624-0297', '243 Queen St S', 'Mississauga', 'ON',
 	'L5M 1L7', 1);
 	
-INSERT INTO shop_brands(shops_id, brands_id) VALUES (1, 1);
-INSERT INTO shop_brands(shops_id, brands_id) VALUES (1, 2);
-INSERT INTO shop_brands(shops_id, brands_id) VALUES (2, 2);
-INSERT INTO shop_brands(shops_id, brands_id) VALUES (2, 3);
-INSERT INTO shop_brands(shops_id, brands_id) VALUES (3, 2);
-INSERT INTO shop_brands(shops_id, brands_id) VALUES (3, 3);
+INSERT INTO shop_brands(shops_id, brands_id) VALUES (1, 33);
+INSERT INTO shop_brands(shops_id, brands_id) VALUES (1, 22);
+INSERT INTO shop_brands(shops_id, brands_id) VALUES (2, 22);
+INSERT INTO shop_brands(shops_id, brands_id) VALUES (2, 9);
+INSERT INTO shop_brands(shops_id, brands_id) VALUES (3, 22);
+INSERT INTO shop_brands(shops_id, brands_id) VALUES (3, 9);
 
 INSERT INTO user_shop_list (owner_id, shop_list_id) VALUES (1, 1);
 INSERT INTO user_shop_list (owner_id, shop_list_id) VALUES (2, 2);
